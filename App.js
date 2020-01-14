e6af0e7c81b14f62b32003911180f57c7f5589d0
John Li <li.johnny34@gmail.com>
2020-01-13 19:28:13 -0800

    Temporary Changes

--- App.js
+++ App.js
@@ -1,24 +1,62 @@
 import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome5'
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'react-native'
+import Amplify from '@aws-amplify/core'
+import {Authenticator} from 'aws-amplify-react-native'
+import awsconfig from './aws-exports'
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
 
+Amplify.configure({
+  ...awsconfig,
+  Analytics: {
+    disabled: true,
+  },
+})
+
+
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+
+        <StatusBar barStyle="dark-content" />
+        <Authenticator usernameAttributes="email" />
+        <Icon name="comments" size={30} color="#900" />
+
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Icon name="comments" size={30} color="#900" />
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
 }
+
+const signUpConfig = {
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 2,
+      type: 'password',
+    },
+  ],
+}
+
 
 const styles = StyleSheet.create({
   container: {
