b7f14e88a410c1aefa12e4f5e0fad17f81624d94
sneakwolf777 <peter.d.chung@gmail.com>
2020-01-27 06:04:39 -0800

    updated search bar

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, useRef } from 'react';
 import {Root} from "native-base";
-import { Button, View, Image, TouchableOpacity } from 'react-native';
+import { View, Image, TouchableOpacity, Dimensions} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import SearchBar from 'react-native-searchbar';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {createStackNavigator} from 'react-navigation-stack';
-import LoginScreen from './Pages/LoginScreen.js';
+import SearchHeader from 'react-native-search-header';
 import HomeScreen from './Pages/HomeScreen.js';
+import LoginScreen from './Pages/LoginScreen.js';
 import FavoriteScreen from './Pages/FavoriteScreen.js';
 import WorkoutsScreen from './Pages/WorkoutsScreen.js';
 import ProgressScreen from './Pages/ProgressScreen.js';
 
+const DEVICE_WIDTH = Dimensions.get(`window`).width;
+
 export default class App extends Component {
   render() {
     return (
+      
       <Root>
         <AppContainer/>
       </Root> 
@@ -23,66 +26,65 @@ export default class App extends Component {
 }
 
 // Bottom navigation to go to Home, Favorite, Workouts, and Progress pages
-const bottomTabNavigator = createBottomTabNavigator(
-  {
-    Home: { 
-      screen: HomeScreen,
-      navigationOptions: {
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Icon
-            name={'home'}
-            size={20}
-            style={{ color: tintColor }}
-          />
-        )
-      }
-    },
-    
-    Favorite: { 
-      screen: FavoriteScreen,
-      navigationOptions: { 
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Icon
-            name={'heart'}
-            size={20}
-            style={{ color: tintColor }}
-          />
-        )
-      }
-    },
-
-    Workouts: {
-      screen: WorkoutsScreen,
-      navigationOptions: {
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Icon
-            name={'calendar-alt'}
-            size={20}
-            style={{ color: tintColor }}
-          />
-        )
-      }
-    },
-
-    Progress: { 
-      screen: ProgressScreen,
-      navigationOptions: {
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Icon
-            name={'chart-line'}
-            size={20}
-            style={{ color: tintColor }}
-          />
+const bottomTabNavigator = createBottomTabNavigator({
+  Home: { 
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Icon
+          name={'home'}
+          size={20}
+          style={{ color: tintColor }}
+        />
+      )
+    }
+  },
+  
+  Favorite: { 
+    screen: FavoriteScreen,
+    navigationOptions: { 
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Icon
+          name={'heart'}
+          size={20}
+          style={{ color: tintColor }}
+        />
       )
     }
   },
 
-}, {
+  Workouts: {
+    screen: WorkoutsScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Icon
+          name={'calendar-alt'}
+          size={20}
+          style={{ color: tintColor }}
+        />
+      )
+    }
+  },
+
+  Progress: { 
+    screen: ProgressScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Icon
+          name={'chart-line'}
+          size={20}
+          style={{ color: tintColor }}
+        />
+      )
+  }
+},
+
+},{
   tabBarOptions: {
     activeTintColor: '#00cccc',
     inactiveTintColor: 'white',
     style: {
-      backgroundColor: '#121212',
+      backgroundColor: 'black',
       borderTopWidth: 0,
       shadowOffset: { width: 5, height: 3 },
       shadowColor: 'black',
@@ -93,59 +95,102 @@ const bottomTabNavigator = createBottomTabNavigator(
   }
 
 },{
-    initialRouteName: 'Home',
-  }
-);
+   initialRouteName: 'Login',
+});
 
 const AppContainer = createAppContainer(createStackNavigator({
-  Login: LoginScreen,
+  Login: { 
+    screen: LoginScreen,
+    navigationOptions: {
+      headerShown: false,
+    } 
+  },
+
   bottomTabNavigator: bottomTabNavigator
+  
   },{
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "#121212",
+  defaultNavigationOptions: {
+    headerTitle: () => null,
+      headerStyle: { 
+        height: 56,
+        backgroundColor: 'black',
         shadowColor: 'black',
         shadowOpacity: 0.5,
         borderBottomWidth: 0
       },
 
-      // Top left header contains TetraBody logo
-      headerLeft: () => 
-        <Image
-          source={ require('./Pages/logo.png') }
-            style={
-            { 
-              flex: 1,
-              height: 20, 
-              width: 100, 
-              marginLeft: 10, 
-              resizeMode: 'contain',
-            }
-          }
-        />,
-      
-      // Top right header contains Search and Account buttons
-      headerRight: () =>
-        <View style={{ flexDirection: 'row', marginRight: 20 }}>
-          <TouchableOpacity style={{ paddingHorizontal: 15 }}>
-            <Icon name='search' size={25} color={'white'} onPress ={() => this.searchBar.show()}/>
+    headerLeft: () => {
 
-              <SearchBar style={{}}
-                ref={(ref) => this.searchBar = ref}
-              />
+    },
 
-          </TouchableOpacity>
+    // Right header contains logo, search button, and account button
+    headerRight: () => {
 
-          <TouchableOpacity style={{ paddingHorizontal: 15 }}>
-            <Icon name='user-circle' size={25} color={'white'} />
-          </TouchableOpacity>
+      const searchHeaderRef = React.createRef(null);
+      return(
+         <View style = {{
+            flex: 1,
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            backgroundColor: 'black'
+         }}>
+           <View style = {{ 
+             flexDirection: 'row',
+             justifyContent: 'flex-end',
+             alignItems: 'center',
+             width: DEVICE_WIDTH,
+             height: 56,
+             marginTop: 7,
+             backgroundColor: 'black',
+             shadowColor: 'black',
+             shadowOpacity: 0.5,
+             borderBottomWidth: 0 
+            }}>
+
+           <Image
+              source={ require('./Pages/logo.png') }
+              style={{ 
+                flex: 1,
+                flexDirection: 'row',
+                height: 23, 
+                width: 100, 
+                marginRight: 190, 
+                resizeMode: 'contain',
+              }}
+           />
+              
+              <TouchableOpacity style={{ paddingHorizontal: 15 }}>
+                <Icon name='search' size={20} color={'white'} onPress = {() => searchHeaderRef.current.show()}/>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{ paddingHorizontal: 15 }}>
+               <Icon name='user-circle' size={20} color={'white'}/>
+              </TouchableOpacity>
+           </View>
+
+           <SearchHeader
+                ref = { searchHeaderRef }
+                placeholder = 'Search...'
+                placeholderColor = 'gray'
+                pinnedSuggestions = {[ `react-native-search-header`, `react-native`, `javascript` ]}
+                onClear = {() => {
+                    console.log(`Clearing input!`);
+                }}
+                onGetAutocompletions = {async (text) => {
+                    if (text) {
+                        const response = await fetch(`http://suggestqueries.google.com/complete/search?client=firefox&q=${text}`, {
+                            method: `get`
+                        });
+                        const data = await response.json();
+                        return data[1];
+                    } else {
+                        return [];
+                    }
+                }}
+             />
         </View>
-
+        );
+      }
     }
-  
-  // App starts at the Login Page
-  },{
-    initialRouteName: 'Login',
   }
-
 ));
